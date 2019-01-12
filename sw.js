f4fd5c29aa9918556b37b916f43d7b6b815728e8
“Jose <jgutierrez.ar@gmail.com>
2019-01-12 10:03:15 +0100

    Sin cache inmutable

--- sw.js
+++ sw.js
@@ -48,11 +48,11 @@ self.addEventListener('install', e => {
     const cacheStatic = caches.open( STATIC_CACHE ).then(cache =>
         cache.addAll( APP_SHELL ));
 
-    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache =>
-        cache.addAll( APP_SHELL_INMUTABLE ));
+    // const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache =>
+    //     cache.addAll( APP_SHELL_INMUTABLE ));
 
 
-    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
+    e.waitUntil( Promise.all([ cacheStatic ]) );
     
 });
 
