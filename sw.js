9e48de91347763fddd17c602f4d444672f56a5cb
“Jose <jgutierrez.ar@gmail.com>
2019-01-13 21:20:06 +0100

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -70,6 +70,12 @@ self.addEventListener('activate', e => {
             if ( key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
+
+            // eliminamos verciones precedentes del dinamico
+            if ( key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
         });
     });
 
